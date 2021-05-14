29f3fb178130f29f84929dbabbd9a82378b5f75b
Rehan Singh <54594678+theRay1s@users.noreply.github.com>
2021-05-14 22:37:07 +0530

    Rebase: with main

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,11 @@
-#!/bin/bash
-
 # sync rom
 repo init -u git://github.com/crdroidandroid/android.git -b 11.0 --depth=1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/theRay1s/android_manifest --depth 1 -b lineage-18.1 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
 source build/envsetup.sh
 brunch merlin
 
-
 # upload rom
-time rclone copy out/target/product/merlin/*.zip cirrus:merlin -P
+rclone copy out/target/product/merlin/crdroidandroid*.zip cirrus:merlin -P
