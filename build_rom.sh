ea5f990444e78aad7fbc40b46bf826d05b34f85c
P-Salik <peersali16@gmail.com>
2021-05-14 23:32:54 +0530

    Corvus-R

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
+
+
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+repo init --depth=1 -u https://github.com/Corvus-R/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 
-# build rom
-source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+git clone https://github.com/P-Salik/local_manifest --depth=1 -b main .repo/local_manifests
 
-# upload rom
-rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build
+. build/envsetup.sh
+lunch corvus_RMX1941-userdebug
+make corvus
+
+# upload
+rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
