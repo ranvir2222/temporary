54430268ad5e81a5747fc0daa821bd0e2ddfefac
BOND007 <mhashimkp@gmail.com>
2021-05-14 19:25:19 +0530

    CrDroid RMX1971

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Hashimkp/local_manifests.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+repo init -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Hashimkp/local_manifests.git --depth 1 -b los .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
 . build/envsetup.sh
-lunch havoc_RMX1971-userdebug
-brunch RMX1971
+lunch lineage_RMX1971-userdebug
+mka bacon -j$(nproc --all)
 
 # upload rom
-rclone copy out/target/product/RMX1971/Havoc-OS*.zip cirrus:RMX1971 -P
+rclone copy out/target/product/RMX1971/crDroid*.zip cirrus:RMX1971 -P
