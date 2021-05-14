9ade019a6f74a34caa840a82439926413e443dfb
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-15 04:39:48 +0600

    May security patch

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # sync rom
 repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests --depth=1
@@ -8,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_mido-user
-m aex -j10
+m aex
 
 # upload rom
 time rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
