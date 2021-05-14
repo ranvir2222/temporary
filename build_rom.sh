e5a15d690db8135a4b9529ffd7bdf3cd166fa66b
LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
2021-05-14 22:04:06 +0300

    daisy: Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -16,3 +16,6 @@ mka bacon
 
 # upload rom los needs *UNOFFICIAL*
 rclone copy out/target/product/daisy/*UNOFFICIAL*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
+
+# Trigger zone
+# 220214052021 - dirty
