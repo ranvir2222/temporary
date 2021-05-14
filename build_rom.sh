1ec2d79eecc93ff77b7b2fa5b5b7d2c4c91523d4
susandahal <susandahal53@gmail.com>
2021-05-14 19:25:02 +0545

    Lavender: Make user build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch styx_lavender-userdebug
+lunch styx_lavender-user
 m styx-ota
 
 # upload rom
