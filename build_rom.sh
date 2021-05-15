ea2d1918c69d396ec05d8f69cb8f0989af96fb2a
rahul9999xda <rahul9999xda@gmail.com>
2021-05-15 11:02:59 +0000

    [TEMP] Test
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,11 @@ repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.gi
 git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+# Test setup
+rm -rf out
+rm -rf device/xiaomi/vayu
+rm -rf vendor/xiaomi/vayu
+
 # build rom
 source build/envsetup.sh
 lunch derp_vayu-userdebug
