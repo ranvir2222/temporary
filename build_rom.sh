6de6a3e559aff1db25fc85b0b00edf3202b7331c
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-05-15 12:39:49 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 -u git://github.com/AOSiP/platform_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone  --depth 1 https://github.com/flashokiller/mainfest_personal -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+repo sync --force-sync --no-tags --no-clone-bundle
 
 # build rom
 source build/envsetup.sh
