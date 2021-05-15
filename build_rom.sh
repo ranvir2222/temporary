eb9210472fc6def03dd8a7a2e89d15c2e05be7c7
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-05-15 11:27:38 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init --depth=1 -u  git://github.com/AOSiP/platform_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone  --depth 1 https://github.com/flashokiller/mainfest_personal.git -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+lunch aosip_ysl-userdebug
+time m kronic
 
 # upload rom
-rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+rclone copy out/target/product/ysl/AOSiP*.zip cirrus:ysl -P
