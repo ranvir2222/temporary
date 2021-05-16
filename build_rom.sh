576b1b95010ff925d8752ddce43c40bdeb649b3b
rahul9999xda <rahul9999xda@gmail.com>
2021-05-16 07:40:11 +0000

    Trigger build
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,10 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/AOSPA/manifest -b ruby -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/derp-sdm660-common/Local-Manifests.git --depth 1 -b aospa .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch derp_vayu-userdebug
-mka derp
+./rom-build.sh X00TD
 
 # upload rom
-rclone copy out/target/product/vayu/DerpFest*.zip cirrus:vayu -P
+rclone copy out/target/product/X00TD/AOSPA*.zip cirrus:X00TD -P
