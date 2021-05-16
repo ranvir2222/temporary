b7c203ec28729ca58f5eb95cdf1e2cad057a8ae2
P-Salik <peersalik16@gmail.com>
2021-05-16 10:53:55 +0530

    Created a Branch [skip ci]

--- build_rom.sh
+++ build_rom.sh
@@ -1,19 +1,12 @@
-#!/bin/bash
-
-
 # sync rom
-repo init --depth=1 -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-git clone https://github.com/P-Salik/local_manifest --depth=1 -b main .repo/local_manifests
+# build rom
+source build/envsetup.sh
+lunch aosp_mido-user
+m aex
 
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-cd ../..
-
-# build
-. build/envsetup.sh
-lunch aosp_RMX1941-userdebug
-mka bacon -j$(nproc --all)
-
-# upload build
-rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
+# upload rom
+rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
