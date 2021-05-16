b333d2a3c053888a068e746f1d2b756b557aba39
Ahmed Elwakil <35235801+PotatoAhmed@users.noreply.github.com>
2021-05-16 18:34:11 +0200

    Change to user build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch corvus_mido-userdebug
+lunch corvus_mido-user
 mka corvus
 
 # upload rom
