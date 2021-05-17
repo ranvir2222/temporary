3479cfd9b528da2646256d8065610c8e4508dd9a
P-Salik <peersalik16@gmail.com>
2021-05-17 09:05:58 +0530

    OctaviOS

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 
 
 # sync rom
-repo init --depth=1 -u git://github.com/CipherOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/Octavi-OS/platform_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 
 git clone https://github.com/P-Salik/local_manifest --depth=1 -b main .repo/local_manifests
 
@@ -12,8 +12,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build
 . build/envsetup.sh
-lunch lineage_RMX1941-userdebug
-mka bacon -j$(nproc --all)
+lunch octavi_RMX1941-userdebug
+brunch RMX1941
 
 # upload build
 rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
