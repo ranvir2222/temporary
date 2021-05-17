4944e6ff1a24267cc572b9e17d0e7e30e4c12c48
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-05-17 17:25:47 +0530

    Nusantara-R

--- build_rom.sh
+++ build_rom.sh
@@ -12,4 +12,4 @@ lunch nad_RMX1941-userdebug
 mka nad
 
 # upload 
-rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
+rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P  
