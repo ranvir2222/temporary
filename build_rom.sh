3e115c02b568c6c0dd76f2f3d71bf2bb7499e0b9
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-05-17 15:39:44 +0530

    Nusantara-R

--- build_rom.sh
+++ build_rom.sh
@@ -1,35 +1,15 @@
 # sync rom
-repo init -u https://github.com/Corvus-R/android_manifest.git -b 11 --depth=1 -g default,-device,-mips,-darwin,-notdefault
+repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 --depth=1 -g default,-device,-mips,-darwin,-notdefault
 
 git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b main .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# patches
-#cd external/selinux
-#curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
-#patch -p1 < *.patch
-#cd ../..
-
-#cd frameworks/base
-#curl -LO https://github.com/PixelExperience/frameworks_base/commit/37f5a323245b0fd6269752742a2eb7aa3cae24a7.patch
-#patch -p1 < *.patch
-#cd ../..
-
-#cd frameworks/opt/net/wifi
-#curl -LO https://github.com/PixelExperience/frameworks_opt_net_wifi/commit/3bd2c14fbda9c079a4dc39ff4601ba54da589609.patch
-#patch -p1 < *.patch
-#cd ../../../..
-
-#cd frameworks/opt/net/ims
-#curl -LO https://github.com/PixelExperience/frameworks_opt_net_ims/commit/661ae9749b5ea7959aa913f2264dc5e170c63a0a.patch
-#patch -p1 < *.patch
-#cd ../../../..
-
 # build
 . build/envsetup.sh
-lunch corvus_RMX1941-userdebug
+export WITH_GAPPS = true
+lunch nad_RMX1941-userdebug
 make corvus
 
 # upload
-rclone copy out/target/product/RMX1941/*Unofficial*.zip cirrus:RMX1941 -P 
+rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P 
