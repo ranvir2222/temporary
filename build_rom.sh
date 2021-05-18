1b4b533110e765e1c23b49b86c19cac173b24f77
susandahal <susandahal53@gmail.com>
2021-05-18 19:15:07 +0545

    Lavender: Build TenX-OS

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 -u https://github.com/StyxProject/manifest -b R -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/susandahal/local_manifest --depth 1 -b styx .repo/local_manifests
+repo init -u git://github.com/TenX-OS/manifest_TenX -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/susandahal/local_manifest --depth 1 -b tenx .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch styx_lavender-user
-m styx-ota
+lunch aosp_lavender-userdebug
+make bacon
 
 # upload rom
-rclone copy out/target/product/lavender/styxOS-*.zip cirrus:lavender -P
+rclone copy out/target/product/lavender/TenX*.zip cirrus:lavender -P
