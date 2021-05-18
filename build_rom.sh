c154e483dc397ce99579787588fd9fb54a5151a0
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-05-18 14:55:38 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 -u git://github.com/AOSiP/platform_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone  --depth 1 https://github.com/flashokiller/mainfest_personal -b master .repo/local_manifests
+git clone  --depth 1 https://github.com/flashokiller/manifest_pine.git -b main .repo/local_manifests
 repo sync --force-sync --no-tags --no-clone-bundle
 
 # build rom
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
-lunch aosip_ysl-eng
+lunch aosip_pine-userdebug
 time m kronic
 
 # upload rom
-rclone copy out/target/product/ysl/AOSiP*.zip cirrus:ysl -P
+rclone copy out/target/product/pine/AOSiP*.zip cirrus:ysl -P
