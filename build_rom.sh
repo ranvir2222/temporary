e28e71b933d7452658e89264259f331d75de828f
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-05-18 10:29:03 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_olivelite-userdebug
+lunch lineage_olivelite-userdebug 
 mka bacon
 
 # upload rom
