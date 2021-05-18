0855c564ef37ddca44dceef20446f60f3c185197
DhruvChhura <73804079+DhruvChhura@users.noreply.github.com>
2021-05-18 10:33:51 +0530

    Trigger Build

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ git clone https://github.com/DhruvChhura/manifest_personal.git --depth 1 -b mast
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Build Rom
+
 source build/envsetup.sh
 lunch derp_ysl-user
 make bacon
