5eff5400b12fd5098f3d367b7a35d46af14a030f
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-05-19 12:31:02 +0530

    Pixel Experience

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
 #cd external/selinux
 #curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
 #patch -p1 < *.patch
-#cd ../..
+#cd ../.. 
 
 #cd frameworks/base
 #curl -LO https://github.com/PixelExperience/frameworks_base/commit/37f5a323245b0fd6269752742a2eb7aa3cae24a7.patch
