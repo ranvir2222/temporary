829206b4922816efb24819395cacc84a9c33c744
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-05-19 13:34:25 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync rom
 repo init --depth=1 -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone  --depth 1 https://github.com/flashokiller/manifest_pine.git -b main .repo/local_manifests
-repo sync --force-sync --no-tags --no-clone-bundle
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 
 # build rom
 source build/envsetup.sh
-# oof 
-export ALLOW_MISSING_DEPENDENCIES=true
 lunch aosp_pine-userdebug
 make bacon
 
