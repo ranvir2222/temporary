ce67a10cd762e47e36207063c624faaf18fbe06d
dustxyz <dustxyz@criptext.com>
2021-05-19 18:08:31 -0400

    fixup! ysl: Build trigger for POSP

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 -u git://github.com/PotatoProject/manifest.git -b dumaloo-release -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b POSP .repo/local_manifests
+git clone https://github.com/Dustxyz/personal_manifest.git --depth 1 -b posp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
