f5698f7420949e714a82ff2fda74e374f61645cd
P-Salik <peersalik16@gmail.com>
2021-05-19 15:09:28 +0530

    Wave-Project | RMX1941

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,15 @@
 
 # sync rom
-repo init --depth=1 -u git://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/Wave-Project/manifest -b r -g default,-device,-mips,-darwin,-notdefault
 
-git clone https://github.com/P-Salik/local_manifest --depth=1 -b DotOS .repo/local_manifests
+git clone https://github.com/P-Salik/local_manifest --depth=1 -b WaveOS .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build
 source build/envsetup.sh
-lunch dot_RMX1941-userdebug
-export WITH_GAPPS=true  # gapps
-make bacon
-
+brunch RMX1941
 
 # upload build
 rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
