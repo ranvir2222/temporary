cf3aeed4aedceee2f40e3d28f1dbdc48fb3fc53b
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-05-19 13:24:33 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AOSiP/platform_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone  --depth 1 https://github.com/flashokiller/manifest_pine.git -b main .repo/local_manifests
 repo sync --force-sync --no-tags --no-clone-bundle
 
@@ -7,8 +7,8 @@ repo sync --force-sync --no-tags --no-clone-bundle
 source build/envsetup.sh
 # oof 
 export ALLOW_MISSING_DEPENDENCIES=true
-lunch aosip_pine-userdebug
-time m kronic
+lunch aosp_pine-userdebug
+make bacon -j8
 
 # upload rom
 rclone copy out/target/product/pine/*.zip cirrus:pine -P
