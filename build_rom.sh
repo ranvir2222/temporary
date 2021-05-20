ff7af75f6624aa023c664d58eddbf2f329185824
Sarthak <sarthakroy2002@gmail.com>
2021-05-20 10:11:05 +0530

    RMX2050 LOS

--- build_rom.sh
+++ build_rom.sh
@@ -12,7 +12,5 @@ export SKIP_API_CHECKS=true
 mka bacon -j$(nproc --all)
 
 # upload rom
-curl -sL https://git.io/file-transfer | sh
-./transfer wet /home/ci/roms/Lineage-FE/out/target/product/RMX2050/*Lineage*.zip
-rclone copy out/target/product/RMX2050/*Lineage*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/RMX2050/*lineage*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
 
