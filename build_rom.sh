2492743b744b3cdea4aac237a89755cfff53d44a
DhruvChhura <73804079+DhruvChhura@users.noreply.github.com>
2021-05-20 12:42:43 +0530

    Trigger Build

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 -u git://github.com/DerpFest-11/manifest.git -b 11 -g defaul
 git clone https://github.com/DhruvChhura/manifest_personal.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # Build Rom
 source build/envsetup.sh
 lunch derp_ysl-user
