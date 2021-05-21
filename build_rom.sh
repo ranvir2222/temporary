4bcaa0c70d86d40531936cdb3068e2bab23f22d1
Debayan <kardebayan3@gmail.com>
2021-05-21 19:58:47 +0530

    CPH1859: Add one patch
    Signed-off-by: Debayan <kardebayan3@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,9 @@ repo init --depth=1 -u https://github.com/StagOS/manifest.git -b r11 -g default,
 git clone https://github.com/kardebayan/local_manifests.git --depth 1 -b r11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+# Patch
+cd system/core && wget https://github.com/HentaiOS-legacy/platform_system_core/commit/51b87d94442fadd886cef02bee1238bc8ea6ea84.patch && git am *.patch && cd ../../
+
 # build rom
 source build/envsetup.sh
 lunch stag_CPH1859-eng
