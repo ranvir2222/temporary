58700afe395c5f184b58f877134092e062a3b94c
MiDoNaSR545 <57400341+MiDoNaSR545@users.noreply.github.com>
2021-05-21 14:05:54 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,12 @@
 # sync rom
-repo init --depth=1 -u https://github.com/Palladium-OS/platform_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/MiDoNaSR545/mainfest_personal --depth 1 -b p .repo/local_manifests
+repo init --depth=1 -u https://github.com/ShapeShiftOS/android_manifest.git -b android_11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/MiDoNaSR545/mainfest_personal --depth 1 -b ssos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch palladium_ysl-user
-make clean
-mka palladium
+lunch ssos_ysl-user
+make bacon
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
