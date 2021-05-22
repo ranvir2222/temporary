ecad5ffa44f935d418550a27d751f116bb9bc796
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-05-22 14:34:47 +0530

    Create build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init -u https://github.com/StyxProject/manifest -b R --depth=1 -g default,-
 git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom      
 source build/envsetup.sh
 lunch styx_RMX1941-userdebug
 m styx-ota
