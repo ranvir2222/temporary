211ca664e08a42a673541a3dc22880e2aed3c150
Aryan Sinha <sinha.aryan03@gmail.com>
2021-05-22 20:25:13 +0530

    olives: Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+
+git clone https://github.com/MinatiScape/local_manifests.git --depth=1 -b main .repo/local_manifests
+
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
-source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+# Build Lineage
+. build/envsetup.sh
+lunch lineage_olives-userdebug
+mka bacon -j$(nproc --all)
 
-# upload rom
-# If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/mido/*.zip cirrus:mido -P
+# Upload Build
+rclone copy out/target/product/olives/*UNOFFICIAL*.zip cirrus:olives -P
