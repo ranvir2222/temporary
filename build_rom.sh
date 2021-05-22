8cd8dfd8f574338dcec6287e46e8996e77aa229b
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-05-22 20:26:06 +0530

    Des XI

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init -u https://github.com/descendant-xi/manifests.git -b eleven-staging --depth=1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom      
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex
-
+lunch descendant_RMX1941-userdebug
+mka descendant
+      
 # upload rom
-# If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/mido/*.zip cirrus:mido -P
+rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
+   
+      
