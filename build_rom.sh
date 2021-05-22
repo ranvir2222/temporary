e865f24249011fd3cef82bff929645a21edcc280
Baibhab <60211800+baibhab34@users.noreply.github.com>
2021-05-23 01:19:57 +0530

    Build stag

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 -u https://github.com/LineageOS/android -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/baibhab34/local_manifest --depth 1 -b los .repo/local_manifests
+repo init -u --depth=1 https://github.com/StagOS/manifest.git -b r11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/baibhab34/local_manifest --depth 1 -b stag .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX1805-userdebug
-mka bacon
+export WITH_GAPPS=true
+lunch stag_RMX1805-userdebug
+mka stag
 
 
 # upload rom
-rclone copy out/target/product/RMX1805/lineage*RMX1805.zip cirrus:RMX1805 -P
+rclone copy out/target/product/RMX1805/StagOS*RMX1805*.zip cirrus:RMX1805 -P
