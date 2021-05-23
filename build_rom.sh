7bf8aa86c1e55b84d3dd0c42479ae35e588bbe7e
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-05-23 08:47:13 +0530

    Palladium-OS

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init -u https://github.com/StyxProject/manifest -b R --depth=1 -g default,-device,-mips,-darwin,-notdefault
+repo init -u https://github.com/Palladium-OS/platform_manifest.git -b 11 --depth=1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom      
 source build/envsetup.sh
-lunch styx_RMX1941-userdebug
-m styx-ota
+lunch palladium_RMX1941-userdebug
+mka palladium
       
 # upload rom
 rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
