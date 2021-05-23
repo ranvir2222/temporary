498ac67831ae6c2a13d38c384b213b3c305837fe
ranvir2222 <84652021+ranvir2222@users.noreply.github.com>
2021-05-23 09:13:49 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init --depth=1 -u git://github.com/CarbonROM/android.git -b cr-9.0 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/ranvir2222/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+lunch carbon_surya-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+make carbon -j$(nproc --all)
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/mido/*.zip cirrus:mido -P
+rclone copy out/target/product/surya/*UNOFFICIAL*.zip cirrus:surya -P
